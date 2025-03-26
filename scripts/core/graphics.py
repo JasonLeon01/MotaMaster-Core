0ec6f1c9d4c7d97c4b11e3cae0c3100efee2a57a
Jason Leon <jasonleon01@outlook.com>
2025-03-27 05:03:29 +0800

    psutil usage and license

--- scripts/core/graphics.py
+++ scripts/core/graphics.py
@@ -111,14 +111,20 @@ class Graphics:
 
     @classmethod
     def debug_info(cls, target: RenderTarget, delta_time: float):
-        import psutil
         if cls._debug_text is None:
             cls._debug_text = Text(System.get_font()[0], '', 12)
 
         fps = 1.0 / delta_time
         average_fps = cls._frame_count / TimeMgr.get_current_time().as_seconds()
-
-        text = f'FPS: {fps:.2f}\nAverage FPS: {average_fps:.2f}\nMemory: {psutil.Process().memory_info().rss / 1024 / 1024:.2f} MB'
+        text = f'FPS: {fps:.2f}\nAverage FPS: {average_fps:.2f}\n'
+        try:
+            import psutil
+            text += f'CPU Usage: {psutil.cpu_percent()}%\n'
+            text += f'CPU Cores: {psutil.cpu_count()}\n'
+            text += f'CPU Frequency: {psutil.cpu_freq().current / 1000:.2f} GHz\n'
+            text += f'Memory Usage:  {psutil.Process().memory_info().rss / 1024 / 1024:.2f} MB\n'
+        except:
+            text += 'Install psutil to get CPU and memory usage.\n'
         cls._debug_text.set_string(text)
         cls._debug_text.set_position(Vector2f(10, 10))
         target.draw(cls._debug_text)
